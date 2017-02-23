03bc8235d5c530f12d7153eb779a680991095f3d
Andrew <aelder@rsgc.on.ca>
2017-02-23 14:18:35 -0500

    Started making the grid class.

--- Binmatch/Binmatch/GameScene.swift
+++ Binmatch/Binmatch/GameScene.swift
@@ -12,7 +12,7 @@ import GameplayKit
 class GameScene: SKScene {
 let player = SKSpriteNode(imageNamed: "Number1")
 let player2 = SKSpriteNode(imageNamed: "Number0")
-let player3 = SKSpriteNode(imageNamed: "Number1")
+
     
     override func didMove(to view: SKView) {
         backgroundColor = SKColor.black
@@ -20,11 +20,17 @@ let player3 = SKSpriteNode(imageNamed: "Number1")
         print(size.width)
         player.position = CGPoint(x: size.width * 0.15, y: size.height * 0.5)
         player2.position = CGPoint(x: size.width * 0.29, y: size.height * 0.5)
-        player3.position = CGPoint(x: size.width * 0.43, y: size.height * 0.5)
+        
+        //var nugget = Grid()
+        
+        //UITouch player = [touches anyObject];
+        //CGPoint location = [touch locationInNode:self];
+        //SKNode *node = [self nodeAtPoint:location];
+
     
         
         addChild(player)
         addChild(player2)
-        addChild(player3)
+
     }
 }
---
+++ Binmatch/Binmatch/Grid.swift
@@ -0,0 +1,21 @@
+//
+//  Title Scene.swift
+//  Binmatch
+//
+//  Created by Andy-E on 2017-02-23.
+//  Copyright © 2017 Andy-E. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class Grid : SKNode {
+    
+    var number1 = SKSpriteNode(imageNamed: "Number1")
+    var number0 = SKSpriteNode(imageNamed: "Number0")
+    
+    
+    
+    
+    
+}
