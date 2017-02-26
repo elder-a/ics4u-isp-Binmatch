d2056ac5ca7feeb9e8f6a927bf06594e91b6d642
Andrew <aelder@rsgc.on.ca>
2017-02-26 13:26:23 -0500

    Able to detect where the clicker clicks! #3

--- Binmatch/Binmatch/GameScene.swift
+++ Binmatch/Binmatch/GameScene.swift
@@ -18,8 +18,8 @@ class GameScene: SKScene {
         backgroundColor = SKColor.black
         
         //print(size.width)
-        player.position = CGPoint(x: size.width * 0.15, y: size.height * 0.5)
-        player2.position = CGPoint(x: size.width * 0.29, y: size.height * 0.5)
+        //player.position = CGPoint(x: size.width * 0.15, y: size.height * 0.5)
+        //player2.position = CGPoint(x: size.width * 0.29, y: size.height * 0.5)
         
         for x in stride(from: 50, through: 400, by: 60){
             // Make sprite objects for various shapes
@@ -47,7 +47,43 @@ class GameScene: SKScene {
         var intX = Int(touch.location(in: player).x)
         var intY = Int(touch.location(in: player).y)
         print("(\(intX) , \(intY)) here are the points in int..")
-       
+        
+        print(findSquare(xVal: intX, yVal: intY))
+        
+    }
+    
+    //-make a funciton that figure out what part of the gird its in
+    func findSquare(xVal: Int, yVal: Int) -> Int {
+        //6x6 grid each square takes up 60x60 pixels
+        //tops stars from 36 goes to 386 and left from 15
+        let startX = 15
+        let startY = 36
+        let incAm = 60
+        var xComp = 0
+        var yComp = 0
+        var squareCount = 0
+        for y in 0...5{
+            for x in 0...5{
+                //check for values here
+                
+                xComp = startX+(incAm*x)//lower boundries
+                yComp = startY+(incAm*y)//lower boundries
+                let xCompUp = xComp+60 //upper boundries
+                let yCompUp = yComp+60
+                
+                if xVal > xComp && xVal < xCompUp {
+                    if yVal > yComp && yVal < yCompUp{
+                        print("Found it")
+                        return squareCount
+                        
+                    }
+                }
+                yComp = 0
+                xComp = 0
+                squareCount += 1
+            }
+        }
+        return squareCount
     }
     
     
@@ -55,39 +91,16 @@ class GameScene: SKScene {
     
     
     
-    //        super.touchesBegan(touches, with: event)
-    //
-    //        let touch: UITouch = (touches.first! as UITouch)
-    //
-    //
-    //        if (touch.view == player.){
     
-    //  }
-    //for i in event {
-    
-    //}
-    //}
+    //-make a funciton that toggles the sprite?
     
     
     
-    //     func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
-    //            let touch:UITouch = touches.anyObject()! as! UITouch
-    //            let positionInScene = touch.location(in: self)
-    //            let touchedNode = self.atPoint(positionInScene)
-    //            print("clicked")
-    //            if let name = touchedNode.name
-    //            {
-    //                if name == "pineapple"
-    //                {
-    //                    print("Touched")
-    //                }
-    //            }
-    //     }
     
     
     
-    //addChild(player)
-    //addChild(player2)
+    
+    
     
     
 }
--- Binmatch/Binmatch/Grid.swift
+++ Binmatch/Binmatch/Grid.swift
@@ -9,11 +9,27 @@
 import SpriteKit
 import GameplayKit
 
-class Grid : SKNode {
+class Grid {
     
     var number1 = SKSpriteNode(imageNamed: "Number1")
     var number0 = SKSpriteNode(imageNamed: "Number0")
     //var rand = arc4random(Int(2))
     
+     init() {
+        //print(size.width)
         
+        for x in stride(from: 50, through: 400, by: 60){
+            // Make sprite objects for various shapes
+            for y in stride(from: 305, through: 605, by: 60) {
+                // Define a square
+                //let  = SKSpriteNode(imageNamed: "Number1")
+                number1.name = "pineapple"
+                number1.position = CGPoint(x: CGFloat(x),
+                                          y: CGFloat(y)) //sets posistion
+                
+                //SKNode.addChild(number1)
+                //SpriteKit.addChild(number1)
+            }
+        }
+    }
 }
