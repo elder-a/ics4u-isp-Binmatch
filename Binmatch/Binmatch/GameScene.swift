436852d382bd2cf4e8b7a86bfae4081feeff26c0
Andrew <aelder@rsgc.on.ca>
2017-02-27 09:13:18 -0500

    Got a grid based on a 2D bool array. #3

--- Binmatch/Binmatch/GameScene.swift
+++ Binmatch/Binmatch/GameScene.swift
@@ -12,15 +12,26 @@ import GameplayKit
 class GameScene: SKScene {
     let player = SKSpriteNode(imageNamed: "Number1")
     let player2 = SKSpriteNode(imageNamed: "Number0")
+    var grid : [[Bool]] = []
     
+    let squareSize = 6 //size of square
     
     override func didMove(to view: SKView) {
         backgroundColor = SKColor.black
+        for _ in 1...squareSize {
+            var gridRow : [Bool] = []
+            for _ in 1...squareSize {
+                var randInt = (Int(arc4random_uniform(2)))
+                if randInt == 0 {
+                    gridRow.append(false)
+                } else {
+                    gridRow.append(true)
+                }
+            }
+            grid.append(gridRow)
+        }
         
-        //print(size.width)
-        //player.position = CGPoint(x: size.width * 0.15, y: size.height * 0.5)
-        //player2.position = CGPoint(x: size.width * 0.29, y: size.height * 0.5)
-        
+        print(grid)
         var countX = 0
         var countY = 0
         for x in stride(from: 50, through: 400, by: 60){
@@ -28,17 +39,29 @@ class GameScene: SKScene {
             // Make sprite objects for various shapes
             for y in stride(from: 305, through: 605, by: 60) {
                 // Define a square
-                let square = SKSpriteNode(imageNamed: "Number1")
-                square.name = "pineapple"
-                square.position = CGPoint(x: CGFloat(x),
-                                          y: CGFloat(y)) //sets posistion
-                self.addChild(square)
+                //square.blendMode
+                let numberOne =  SKSpriteNode(imageNamed: "Number1")
+                let numberZero =  SKSpriteNode(imageNamed: "Number0")
+                
+                numberOne.position = CGPoint(x: CGFloat(x),
+                                             y: CGFloat(y)) //sets posistion
+                
+                numberZero.position = CGPoint(x: CGFloat(x),
+                                              y: CGFloat(y)) //sets posistion
+                
+                
+                if countX < 6 && countY < 6{
+                    if grid[countY][countX] == true {
+                        self.addChild(numberOne)
+                    } else {
+                        self.addChild(numberZero)
+                    }
+                }
                 countY += 1
             }
             countX += 1
         }
         print("\(countX) , \(countY) Counts")
-        
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -75,7 +98,8 @@ class GameScene: SKScene {
                 if xVal == countX && yVal == countY {
                     let  secondNumber = SKSpriteNode(imageNamed: "Number0")
                     secondNumber.position = CGPoint(x: CGFloat(x),
-                                              y: CGFloat(y)) //sets posistion
+                                                    y: CGFloat(y)) //sets posistion
+                    
                     //square.
                     self.addChild(secondNumber) //should add to screen
                     print("\(countX) , \(countY) LET INTO Statment")
