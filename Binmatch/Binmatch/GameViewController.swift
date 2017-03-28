8f11e18fb163a2f2a0152e9356456e6acc101e41
Andrew <aelder@rsgc.on.ca>
2017-03-28 14:04:19 -0400

    Got a initial version of the game working.

--- Binmatch/Binmatch/GameViewController.swift
+++ Binmatch/Binmatch/GameViewController.swift
@@ -13,6 +13,7 @@ class GameViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         let scene = GameScene(size: view.bounds.size)
         let skView = view as! SKView
         skView.showsFPS = true
---
+++ Binmatch/Binmatch/TitleScene.swift
@@ -0,0 +1,22 @@
+//
+//  TitleScene.swift
+//  Binmatch
+//
+//  Created by Andy-E on 2017-03-28.
+//  Copyright © 2017 Andy-E. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+
+class TitleScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+    }
+    
+    
+    
+    
+}
